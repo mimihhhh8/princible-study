24800b1942a22efea43c2456e111719a6c8dcfd8
guorong <guorong@tuyoogame.com>
2022-03-17 16:48:03 +0800

    vuex

--- src/store/index.js
+++ src/store/index.js
@@ -1,8 +1,14 @@
 import { createStore } from "vuex";
 
 export default createStore({
-  state: {},
-  mutations: {},
+  state: {
+    count: 0,
+  },
+  mutations: {
+    increment(state) {
+      state.count++;
+    },
+  },
   actions: {},
   modules: {},
 });
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png" />
+    <button @click="handleClick">点击</button>
   </div>
 </template>
 
@@ -10,5 +11,12 @@
 export default {
   name: "Home",
   components: {},
+
+  methods: {
+    handleClick() {
+      this.$store.commit("increment");
+      console.log(this.$store.state.count, "hhahah");
+    },
+  },
 };
 </script>
